811b73c84a9d34a36739427abf396280c324b12b
BjornFJohansson <bjornjobb@gmail.com>
2025-01-25 15:11:07 +0000

    this file seems changing back and forth?

--- scripts/guess_alphabet.py
+++ scripts/guess_alphabet.py
@@ -1,125 +1,128 @@
-#!/usr/bin/env python
 # -*- coding: utf-8 -*-
-def guess_alphabet(sequence):
-    """
-    guess_alphabet(sequence)
+# #!/usr/bin/env python
+# # -*- coding: utf-8 -*-
 
-    returns an apropriate Biopython alphabet based on the content of the sequence.
 
-    >>> from pydna.dsdna import guess_alphabet
-    >>> guess_alphabet("aaa")
-    IUPACUnambiguousDNA()
-    >>> guess_alphabet("bbb")
-    IUPACProtein()
-    >>> guess_alphabet("aaaac")
-    IUPACUnambiguousDNA()
-    >>> guess_alphabet("agtc")
-    IUPACUnambiguousDNA()
-    >>> guess_alphabet("aguc")
-    IUPACUnambiguousRNA()
-    >>> guess_alphabet("ALAASN")
-    'three letter code'
+# def guess_alphabet(sequence):
+#     """
+#     guess_alphabet(sequence)
 
-    """
-    # SeqRecord or Dseqrecord?
-    if hasattr(sequence, "features"):
-        sequence = str(sequence.seq)
+#     returns an apropriate Biopython alphabet based on the content of the sequence.
 
-    # Seq or Dseq ?
-    elif hasattr(sequence, "reverse_complement"):
-        sequence = str(sequence)
+#     >>> from pydna.dsdna import guess_alphabet
+#     >>> guess_alphabet("aaa")
+#     IUPACUnambiguousDNA()
+#     >>> guess_alphabet("bbb")
+#     IUPACProtein()
+#     >>> guess_alphabet("aaaac")
+#     IUPACUnambiguousDNA()
+#     >>> guess_alphabet("agtc")
+#     IUPACUnambiguousDNA()
+#     >>> guess_alphabet("aguc")
+#     IUPACUnambiguousRNA()
+#     >>> guess_alphabet("ALAASN")
+#     'three letter code'
 
-    else:
-        if not isinstance(sequence, str):
-            warnings.warn(
-                ("Input is {}, expected string, " "unicode string, Seq or SeqRecord " "objects!").format(
-                    type(sequence)
-                )
-            )
-        sequence = str(sequence)
+#     """
+#     # SeqRecord or Dseqrecord?
+#     if hasattr(sequence, "features"):
+#         sequence = str(sequence.seq)
 
-    if len(sequence) < 1:
-        return SingleLetterAlphabet()
+#     # Seq or Dseq ?
+#     elif hasattr(sequence, "reverse_complement"):
+#         sequence = str(sequence)
 
-    for c in sequence:
-        if c not in string.printable:
-            return SingleLetterAlphabet()
+#     else:
+#         if not isinstance(sequence, str):
+#             warnings.warn(
+#                 ("Input is {}, expected string, " "unicode string, Seq or SeqRecord " "objects!").format(
+#                     type(sequence)
+#                 )
+#             )
+#         sequence = str(sequence)
 
-    xp = set(extended_protein.letters)
-    pr = set(protein.letters)
+#     if len(sequence) < 1:
+#         return SingleLetterAlphabet()
 
-    ad = set(ambiguous_dna.letters)
-    ud = set(unambiguous_dna.letters)
-    ed = set(extended_dna.letters)
+#     for c in sequence:
+#         if c not in string.printable:
+#             return SingleLetterAlphabet()
 
-    ar = set(ambiguous_rna.letters)
-    ur = set(unambiguous_rna.letters)
+#     xp = set(extended_protein.letters)
+#     pr = set(protein.letters)
 
-    all = xp | pr | ad | ud | ed | ar | ur
+#     ad = set(ambiguous_dna.letters)
+#     ud = set(unambiguous_dna.letters)
+#     ed = set(extended_dna.letters)
 
-    sequence_chars = set(sequence.upper())
+#     ar = set(ambiguous_rna.letters)
+#     ur = set(unambiguous_rna.letters)
 
-    if sequence_chars - all - set(string.punctuation + string.whitespace):
-        return SingleLetterAlphabet()
+#     all = xp | pr | ad | ud | ed | ar | ur
 
-    nucleic_count = 0
+#     sequence_chars = set(sequence.upper())
 
-    for letter in "GATCUNgatcun":
-        nucleic_count += sequence.count(letter)
+#     if sequence_chars - all - set(string.punctuation + string.whitespace):
+#         return SingleLetterAlphabet()
 
-    if float(nucleic_count) / float(len(sequence)) >= 0.9:  # DNA or RNA
-        if "T" in sequence_chars and "U" in sequence_chars:
-            alphabet = NucleotideAlphabet()
-        elif not sequence_chars - ud:
-            alphabet = unambiguous_dna
-        elif not sequence_chars - ad:
-            alphabet = ambiguous_dna
-        elif not sequence_chars - ed:
-            alphabet = extended_dna
-        elif not sequence_chars - ur:
-            alphabet = unambiguous_rna
-        elif not sequence_chars - ar:
-            alphabet = extended_rna
-        else:
-            alphabet = NucleotideAlphabet()
-    else:
-        threecode = [
-            "ALA",
-            "ASX",
-            "CYS",
-            "ASP",
-            "GLU",
-            "PHE",
-            "GLY",
-            "HIS",
-            "ILE",
-            "LYS",
-            "LEU",
-            "MET",
-            "ASN",
-            "PRO",
-            "GLN",
-            "ARG",
-            "SER",
-            "THR",
-            "VAL",
-            "TRP",
-            "TYR",
-            "GLX",
-            "XAA",
-            "TER",
-            "SEL",
-            "PYL",
-            "XLE",
-        ]
-        tc = set(threecode)
-        three_letter_alphabet = set([sequence[i : i + 3] for i in range(0, len(sequence), 3)])
-        if not three_letter_alphabet - tc:
-            alphabet = "three letter code"
-        elif sequence_chars - pr:
-            alphabet = protein
-        elif sequence_chars - xp:
-            alphabet = extended_protein
-        else:
-            alphabet = ProteinAlphabet()
-    return alphabet
+#     nucleic_count = 0
+
+#     for letter in "GATCUNgatcun":
+#         nucleic_count += sequence.count(letter)
+
+#     if float(nucleic_count) / float(len(sequence)) >= 0.9:  # DNA or RNA
+#         if "T" in sequence_chars and "U" in sequence_chars:
+#             alphabet = NucleotideAlphabet()
+#         elif not sequence_chars - ud:
+#             alphabet = unambiguous_dna
+#         elif not sequence_chars - ad:
+#             alphabet = ambiguous_dna
+#         elif not sequence_chars - ed:
+#             alphabet = extended_dna
+#         elif not sequence_chars - ur:
+#             alphabet = unambiguous_rna
+#         elif not sequence_chars - ar:
+#             alphabet = extended_rna
+#         else:
+#             alphabet = NucleotideAlphabet()
+#     else:
+#         threecode = [
+#             "ALA",
+#             "ASX",
+#             "CYS",
+#             "ASP",
+#             "GLU",
+#             "PHE",
+#             "GLY",
+#             "HIS",
+#             "ILE",
+#             "LYS",
+#             "LEU",
+#             "MET",
+#             "ASN",
+#             "PRO",
+#             "GLN",
+#             "ARG",
+#             "SER",
+#             "THR",
+#             "VAL",
+#             "TRP",
+#             "TYR",
+#             "GLX",
+#             "XAA",
+#             "TER",
+#             "SEL",
+#             "PYL",
+#             "XLE",
+#         ]
+#         tc = set(threecode)
+#         three_letter_alphabet = set([sequence[i : i + 3] for i in range(0, len(sequence), 3)])
+#         if not three_letter_alphabet - tc:
+#             alphabet = "three letter code"
+#         elif sequence_chars - pr:
+#             alphabet = protein
+#         elif sequence_chars - xp:
+#             alphabet = extended_protein
+#         else:
+#             alphabet = ProteinAlphabet()
+#     return alphabet
