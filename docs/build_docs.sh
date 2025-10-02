1099f0a3ec5726383ac1c7b8003d002bd647d88d
Manuel Lera-Ramirez <manulera14@gmail.com>
2025-10-02 13:43:39 +0100

    dont fail sphinx on warning

--- docs/build_docs.sh
+++ docs/build_docs.sh
@@ -1,3 +1,3 @@
 rm -rf _build
 sphinx-apidoc --force --implicit-namespaces --module-first -o reference ../src/pydna
-sphinx-build -n -W --keep-going -b html ./ ./_build/
+sphinx-build -n --keep-going -b html ./ ./_build/
