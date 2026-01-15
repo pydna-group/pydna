86ccb821ca23e8e9ff99dcf4d348310c8d9493a8
BjornFJohansson <bjornjobb@gmail.com>
2026-01-15 08:37:51 +0000

    added test files

--- src/pydna/contig.py
+++ src/pydna/contig.py
@@ -4,6 +4,7 @@ import networkx as _nx
 from pydna._pretty import pretty_str as _pretty_str
 from pydna.dseqrecord import Dseqrecord as _Dseqrecord
 from pydna.utils import rc as _rc
+import numpy as np
 
 
 class Contig(_Dseqrecord):
@@ -263,3 +264,195 @@ class Contig(_Dseqrecord):
             fig += "|{space}   |\n".format(space=" " * (space))
             fig += " {space}".format(space="-" * (space + 3))
         return _pretty_str(_textwrap.dedent(fig))
+
+    def figure_mpl(self):
+        """
+        Graphic representation of the assembly.
+
+        Returns
+        -------
+        matplotlib.figure.Figure
+            A representation of a linear or culrcular assembly.
+
+        """
+        # lazy imports in case matplotlib is not installed
+        import matplotlib.pyplot as plt
+        import matplotlib.patches as mpatches
+
+        plt.ioff()  # Disable interactive mode, otherwise two plots are shown in Spyder.
+        # https://matplotlib.org/stable/api/_as_gen/matplotlib.pyplot.isinteractive.html#matplotlib.pyplot.isinteractive
+
+        def pick_n_colors(n, cmap_name="tab20"):
+            cmap = plt.get_cmap(cmap_name)
+            return [cmap(i / n) for i in range(n)]
+
+        fig, ax = plt.subplots()
+        edges = list(self.graph.edges(data=True))
+        colors = pick_n_colors(len(edges))
+
+        if self.circular:
+            # Circle parameters for Circular assembly
+            center = 0, 0
+            outer_radius = 1.5  # fragments on the outer lane
+            middle_radius = 1.3  # fragments on the inner lane
+            small_radius = 1.1  # odd number of fragments require an extra radius
+            arc_width = 0.1  # Arc thickness
+
+            circle = len(self)  # The circle has the length of the assembly
+            radii = [outer_radius, middle_radius] * (
+                len(edges) // 2
+            )  # radii alternates, starting with outer.
+
+            if len(edges) % 2 != 0:  # last fragment get a smaller radius
+                radii.append(small_radius)
+
+            assert (
+                len(colors) == len(radii) == len(edges)
+            )  # One color and one radius for each edge.
+
+            # The recombination between last and first fragments
+            # end at the origin (twelve o'clock).
+            start = 0 - len(edges[0][0])
+
+            for edge, radius, color in zip(edges, radii, colors):
+
+                node1, node2, meta = edge
+                slc = meta["piece"]
+                extra = len(node2)
+                # slc contain the first but not the second node, so add extra to the length
+                length = slc.stop - slc.start + extra
+
+                theta1 = 90.0 - 360.0 / circle * start
+                theta2 = 90.0 - 360.0 / circle * (start + length)
+
+                # Create arc
+                arc_patch = mpatches.Wedge(
+                    center=center,
+                    r=radius,
+                    theta1=theta2,
+                    theta2=theta1,
+                    width=arc_width,
+                    edgecolor=color,
+                    facecolor=(1, 1, 1, 0),
+                    linewidth=1,
+                )
+                ax.add_patch(arc_patch)
+
+                # Compute label position slightly outside the arc
+                mid_angle = (theta1 + theta2) / 2
+                rad = np.deg2rad(mid_angle)
+                label_radius = radius + arc_width + 0.1  # place outside the arc
+                x = label_radius * np.cos(rad)
+                y = label_radius * np.sin(rad)
+
+                # Choose alignment based on angle
+                ha = "left" if np.cos(rad) >= 0 else "right"
+                va = "center"
+
+                ax.text(x, y, meta["name"], ha=ha, va=va, fontsize=10)
+
+                start += length - len(node2)
+            ax.axis("off")
+            ax.set_aspect("equal")
+            ax.set_xlim(-1.6, 1.6)  # This should be enough, but not extensively tested.
+            ax.set_ylim(-1.6, 1.6)
+
+        else:  # Linear assembly
+            import itertools  # 3131 bp
+
+            unit = len(self) / 50
+            upper = 4 * unit
+            lower = 1 * unit
+            height = 1 * unit
+            x = 0
+
+            for edge, y, color in zip(edges, itertools.cycle((lower, upper)), colors):
+                node1, node2, metadict = edge
+                slc = metadict["piece"]
+                # slc contain the first but not the second node, so add extra to the length if not begin or end.
+                extra = len(node2) if node2 not in ("begin", "end") else 0
+                length = slc.stop - slc.start + extra
+                box = mpatches.FancyBboxPatch(
+                    (x, y),
+                    length,
+                    height,
+                    linewidth=1,
+                    boxstyle="round",
+                    edgecolor=color,
+                    facecolor=(1, 1, 1, 0),
+                )
+                ax.add_patch(box)
+                ax.text(
+                    x + length / 2,
+                    y + height * 2 if y == upper else y - height * 2,
+                    metadict["name"],
+                    ha="center",
+                    va="center",
+                    fontsize=10,
+                )
+                x += length - len(node2)
+            ax.axis("off")
+            ax.set_aspect("equal")
+            ax.set_xlim(-1, len(self) + 1)
+            ax.set_ylim(-height, height * 2 + upper)
+        return fig
+
+    # FIXME: This code uses plotly, but I see no reason for it at this point.
+    # def figure_plotly(self):
+    #     import plotly.graph_objects as go
+    #     import numpy as np
+
+    #     circ = len(self)
+    #     arcs = list(self.graph.edges(data=True))
+
+    #     # Radii setup
+    #     small_radius = 1.1
+    #     middle_radius = 1.3
+    #     outer_radius = 1.5
+    #     arc_width = 0.1
+
+    #     radii = [outer_radius, middle_radius] * (len(arcs) // 2)
+    #     if len(arcs) % 2 != 0:
+    #         radii.append(small_radius)
+
+    #     fig = go.Figure()
+    #     start = 0 - len(arcs[0][0])
+
+    #     for (node1, node2, meta), radius in zip(arcs, radii):
+    #         slc = meta["piece"]
+    #         length = slc.stop - slc.start + len(node1)
+
+    #         theta1 = 90.0 - 360.0 / circ * start
+    #         theta2 = 90.0 - 360.0 / circ * (start + length)
+
+    #         # Generate arc points
+    #         theta = np.linspace(theta1, theta2, 50)
+    #         theta_rev = theta[::-1]
+
+    #         r_outer = np.full_like(theta, radius)
+    #         r_inner = np.full_like(theta_rev, radius - arc_width)
+
+    #         r = np.concatenate([r_outer, r_inner])
+    #         t = np.concatenate([theta, theta_rev])
+
+    #         fig.add_trace(
+    #             go.Scatterpolar(
+    #                 r=r,
+    #                 theta=t,
+    #                 fill="toself",
+    #                 mode="lines",
+    #                 line_color="rgba(0,100,200,0.6)",
+    #                 hoverinfo="text",
+    #                 text=meta["name"],
+    #                 name=meta["name"],
+    #             )
+    #         )
+
+    #         start += length - len(node2)
+
+    #     fig.update_layout(
+    #         polar=dict(radialaxis=dict(visible=False), angularaxis=dict(visible=False)),
+    #         showlegend=False,
+    #     )
+
+    #     fig.show("browser")
---
+++ tests/test_module_contig2.py
@@ -0,0 +1,300 @@
+# -*- coding: utf-8 -*-
+import pytest
+from pydna.dseqrecord import Dseqrecord
+from pydna.assembly import Assembly
+from pydna import contig
+from pydna._pretty import pretty_str
+from pydna.amplify import pcr
+from pydna.parsers import parse_primers
+from pydna.readers import read
+
+
+a = Dseqrecord("acgatgctatactgCCCCCtgtgctgtgctcta", name="one")
+b = Dseqrecord("tgtgctgtgctctaTTTTTtattctggctgtatc", name="two")
+c = Dseqrecord("tattctggctgtatcGGGGGtacgatgctatactg", name="three")
+
+
+@pytest.mark.mpl_image_compare
+def test_contig_linear():
+
+    asm = Assembly((a, b, c), limit=14)
+
+    cont = asm.assemble_linear()[0]
+
+    fig = (
+        "one|14\n" "    \\/\n" "    /\\\n" "    14|two|15\n" "           \\/\n" "           /\\\n" "           15|three"
+    )
+
+    assert fig == cont.figure()
+
+    assert repr(cont) == "Contig(-73)"
+
+    assert cont._repr_html_() == f"<pre>{fig}</pre>"
+
+    assert cont.detailed_figure() == (
+        "acgatgctatactgCCCCCtgtgctgtgctcta\n"
+        "                   TGTGCTGTGCTCTA\n"
+        "                   tgtgctgtgctctaTTTTTtattctggctgtatc\n"
+        "                                      TATTCTGGCTGTATC\n"
+        "                                      tattctggctgtatcGGGGGtacgatgctatactg\n"
+    )
+
+    return cont.figure_mpl()
+
+
+@pytest.mark.mpl_image_compare
+def test_contig_circular():
+
+    asm = Assembly((a, b, c), limit=14)
+
+    cont = asm.assemble_circular()[0]
+
+    assert repr(cont) == "Contig(o59)"
+
+    assert cont.detailed_figure() == (
+        "||||||||||||||\n"
+        "acgatgctatactgCCCCCtgtgctgtgctcta\n"
+        "                   TGTGCTGTGCTCTA\n"
+        "                   tgtgctgtgctctaTTTTTtattctggctgtatc\n"
+        "                                      TATTCTGGCTGTATC\n"
+        "                                      tattctggctgtatcGGGGGtacgatgctatactg\n"
+        "                                                           ACGATGCTATACTG\n"
+    )
+
+    fig = (
+        " -|one|14\n"
+        "|      \\/\n"
+        "|      /\\\n"
+        "|      14|two|15\n"
+        "|             \\/\n"
+        "|             /\\\n"
+        "|             15|three|14\n"
+        "|                      \\/\n"
+        "|                      /\\\n"
+        "|                      14-\n"
+        "|                         |\n"
+        " -------------------------"
+    )
+
+    assert fig == cont.figure()
+
+    return cont.figure_mpl()
+
+    from unittest.mock import MagicMock
+
+    pp = MagicMock()
+
+    cont._repr_pretty_(pp, None)
+
+    pp.text.assert_called_with("Contig(o59)")
+
+    from Bio.Seq import Seq
+
+    from pydna.seqrecord import SeqRecord
+
+    arg = SeqRecord(Seq("aaa"))
+
+    import networkx as nx
+
+    x = contig.Contig.from_SeqRecord(arg, graph=nx.MultiDiGraph())
+
+
+def test_reverse_complement():
+
+    a = Dseqrecord("acgatgctatactgtgCCNCCtgtgctgtgctcta")
+    # 12345678901234
+    b = Dseqrecord("tgtgctgtgctctaTTTTTTTtattctggctgtatc")
+    # 123456789012345
+    c = Dseqrecord("tattctggctgtatcGGGGGtacgatgctatactgtg")
+    a.name = "aaa"  # 1234567890123456
+    b.name = "bbb"
+    c.name = "ccc"
+    asm = Assembly((a, b, c), limit=14)
+    x = asm.assemble_circular()[0]
+    y = x.rc()
+    z = y.rc()
+    assert x.figure() == z.figure()
+    assert x.detailed_figure() == z.detailed_figure()
+
+    xfig = """\
+ -|aaa|14
+|      \\/
+|      /\\
+|      14|bbb|15
+|             \\/
+|             /\\
+|             15|ccc|16
+|                    \\/
+|                    /\\
+|                    16-
+|                       |
+ -----------------------
+     """.rstrip()
+
+    xdfig = pretty_str(
+        """\
+||||||||||||||||
+acgatgctatactgtgCCNCCtgtgctgtgctcta
+                     TGTGCTGTGCTCTA
+                     tgtgctgtgctctaTTTTTTTtattctggctgtatc
+                                          TATTCTGGCTGTATC
+                                          tattctggctgtatcGGGGGtacgatgctatactgtg
+                                                               ACGATGCTATACTGTG
+    """.rstrip()
+        + "\n"
+    )
+
+    assert x.figure() == xfig
+    assert x.detailed_figure() == xdfig
+
+    yfig = """\
+ -|ccc_rc|15
+|         \\/
+|         /\\
+|         15|bbb_rc|14
+|                   \\/
+|                   /\\
+|                   14|aaa_rc|16
+|                             \\/
+|                             /\\
+|                             16-
+|                                |
+ --------------------------------
+     """.rstrip()
+
+    ydfig = (
+        """\
+||||||||||||||||
+cacagtatagcatcgtaCCCCCgatacagccagaata
+                      GATACAGCCAGAATA
+                      gatacagccagaataAAAAAAAtagagcacagcaca
+                                            TAGAGCACAGCACA
+                                            tagagcacagcacaGGNGGcacagtatagcatcgt
+                                                               CACAGTATAGCATCGT
+    """.rstrip()
+        + "\n"
+    )
+
+    assert y.figure() == yfig
+    assert y.detailed_figure() == ydfig
+
+
+def test_linear():
+
+    a = Dseqrecord("acgatgctatactgtgCCNCCtgtgctgtgctcta")
+    # 12345678901234
+    b = Dseqrecord("tgtgctgtgctctaTTTTTTTtattctggctgtatc")
+    # 123456789012345
+    c = Dseqrecord("tattctggctgtatcGGGGGtacgatgctatactgtg")
+    a.name = "aaa"  # 1234567890123456
+    b.name = "bbb"
+    c.name = "ccc"
+    asm = Assembly((a, b, c), limit=14)
+    x = asm.assemble_linear()[0]
+
+    answer = "aaa|14\n    \\/\n    /\\\n    14|bbb|15\n           \\/\n           /\\\n           15|ccc"
+
+    assert x.figure() == answer.strip()
+    answer = "acgatgctatactgtgCCNCCtgtgctgtgctcta\n                     TGTGCTGTGCTCTA\n                     tgtgctgtgctctaTTTTTTTtattctggctgtatc\n                                          TATTCTGGCTGTATC\n                                          tattctggctgtatcGGGGGtacgatgctatactgtg\n"
+    assert x.detailed_figure()
+
+
+@pytest.mark.mpl_image_compare
+def test_mpl1():
+
+    a = Dseqrecord("GCCGATTCTCATCCGGGCCT" "CACTATAGGACCATTCCGTT" "GCCCTGCGCTGCGCTGTATA", name="1")
+
+    b = Dseqrecord("GCCCTGCGCTGCGCTGTATA" "TCCCCAGGAACAGACTTCCT" "GCCGATTCTCATCCGGGCCT", name="2")
+
+    asm = Assembly((a, b), limit=20)
+    cps = asm.assemble_circular()
+    cp = cps[0]
+    return cp.figure_mpl()
+
+
+@pytest.mark.mpl_image_compare
+def test_mpl2():
+
+    x = Dseqrecord("CTAAGATATTCTTACGTGTA" "CACTATAGGACCATTCCGTT" "GCCCTGCGCTGCGCTGTATA", name="x")
+
+    y = Dseqrecord("GCCCTGCGCTGCGCTGTATA" "TCCCCAGGAACAGACTTCCT" "GCCGATTCTCATCCGGGCCT", name="y")
+
+    z = Dseqrecord("GCCGATTCTCATCCGGGCCT" "GGATGCAATGCGATCCTCCG" "CTAAGATATTCTTACGTGTA", name="z")
+
+    asm2 = Assembly((x, y, z), limit=20)
+    cps2 = asm2.assemble_circular()
+    cp2 = cps2[0]
+    return cp2.figure_mpl()
+
+
+@pytest.mark.mpl_image_compare
+def test_mpl3():
+
+    p = {}
+
+    p[1113], p[987], p[1196], p[1195], p[978], p[977], p[984], p[983], p[1804], p[1347] = parse_primers(
+        '''
+
+    >1113_Amp.fw.nw 55-mer
+    GAAAAGCGTTTACCTCGGAACTCTATTGTAGAACCCCTATTTGTTTATTTTTCTA
+
+    >987_Amp.REV 52-merAmp.REV
+    AGAAAGTCTACACCTTACGCTGATTGGATTTGAAGTTTTAAATCAATCTAAA
+
+    >1196_Pbr.FW 49-mer
+    AATCCAATCAGCGTAAGGTGTAGACTTTCTCTGTCAGACCAAGTTTACT
+
+    >1195_Pbr.REV 44-mer
+    GTTGACTACTATTTACGCAGCAGATACGATCTCGTTTCATCGGT
+
+    >978_Crp.FW 46-merCrp.FW
+    AACTGTAAAATCAGGTATCTCGTAGTCCGTGTTCTGATCCTCGAGC
+
+    >977_Crp.REV 47-merCrp.REV
+    TACAATAGAGTTCCGAGGTAAACGCTTTTCGTTCTTGTCTCATTGCC
+
+    >984_Micron.FW 49-merMicron.FW
+    ATCGTATCTGCTGCGTAAATAGTAGTCAACGATCGTACTTGTTACCCAT
+
+    >983_Micron.REV 48-merMicron.REV
+    CAGAGCAGACAGTTCCTTTACGAGATTTTAGATCCAATATCAAAGGAA
+
+    >1804_TRP1fp_pTA 51-merreplaces 1348 & 1680
+    TAAAATCTCGTAAAGGAACTGTCTGCTCTGtataaaaataggcgtatcacg
+
+    >1347_TRP1rp_pTA 51-mer
+    ACGGACTACGAGATACCTGATTTTACAGTTGATCTTTTATGCTTGCTTTTC
+
+    '''
+    )
+
+    lol = [
+        ['pBR322.gb', 1113, 987, 'amp'],
+        ['pBR322.gb', 1196, 1195, 'pBR'],
+        ['pYPKpw.gb', 978, 977, 'Δcrp'],
+        ['YEplac195_snapgene.gb', 984, 983, '2µ'],
+        ['YIplac204_snapgene.gb', 1804, 1347, 'TRP1'],
+    ]
+
+    fragments = []
+
+    for row in lol:
+        template, fp, rp, target = row
+        fp = p[int(fp)]
+        rp = p[int(rp)]
+        template = read(template.strip())
+        fragment = pcr(fp, rp, template)
+        fragment.name = target.strip()
+        fragments.append(fragment)
+
+    asm = Assembly(fragments)
+
+    cps = asm.assemble_circular()
+
+    cp = cps[0]
+
+    return cp.figure_mpl()
+
+
+if __name__ == "__main__":
+    pytest.main([__file__, "-vv", "-s"])
