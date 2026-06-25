da72b6733a8c6170545e9620fa47e759825017b5
BjornFJohansson <bjornjobb@gmail.com>
2026-06-25 07:16:37 +0100

    Added findall function that finds strings in other strings despite substitutions, insertions and deletions. depends on edlib https://github.com/Martinsos/edlib.

---
+++ tests/test_module_findall.py
@@ -0,0 +1,152 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+from pydna.findall import findall
+
+
+def test_find_with_deletion():
+    # ACGT-ACGT
+    # ||||-||||
+    # ACGTTACGT
+    needle = "ACGTACGT"
+    haystack = "TTTACGTTACGTAAA"
+
+    results = findall(needle, haystack, 1)
+
+    assert results == [
+        {
+            "distance": 1,
+            "start": 3,
+            "stop": 12,
+            "cigar": "4=1D4=",
+            "alignment": "ACGT-ACGT\n||||-||||\nACGTTACGT",
+        }
+    ]
+
+
+def test_find_with_insert():
+    # ACGTTACGT
+    # ||||-||||
+    # ACGT-ACGT
+    needle = "ACGTTACGT"
+    haystack = "TTTACGTACGTAAA"
+
+    results = findall(needle, haystack, 1, circular=False)
+
+    assert results == [
+        {
+            "distance": 1,
+            "start": 3,
+            "stop": 11,
+            "cigar": "4=1I4=",
+            "alignment": "ACGTTACGT\n||||-||||\nACGT-ACGT",
+        }
+    ]
+
+
+def test_find_with_sub():
+    # ACGTTACGT
+    # ||||.||||
+    # ACGTCACGT
+    needle = "ACGTTACGT"
+    haystack = "TTTACGTCACGTAAA"
+
+    results = findall(needle, haystack, 1, circular=False)
+
+    assert results == [
+        {
+            "distance": 1,
+            "start": 3,
+            "stop": 12,
+            "cigar": "4=1X4=",
+            "alignment": "ACGTTACGT\n||||.||||\nACGTCACGT",
+        }
+    ]
+
+
+def test_find_with_insert_deletion():
+    # ACGT-ACAGTTT
+    # ||||-||-||||
+    # ACGTTAC-GTTT
+    needle = "ACGTACAGTTT"
+    haystack = "TTTACGTTACGTTTAAA"
+
+    results = findall(needle, haystack, 2, circular=False)
+
+    assert results == [
+        {
+            "distance": 2,
+            "start": 3,
+            "stop": 14,
+            "cigar": "4=1D2=1I4=",
+            "alignment": "ACGT-ACAGTTT\n||||-||-||||\nACGTTAC-GTTT",
+        }
+    ]
+
+
+def test_find_across_circular_origin():
+    needle = "TAAC"
+    haystack = "ACGTTA"
+
+    results = findall(
+        needle,
+        haystack,
+        max_edits=0,
+        circular=True,
+    )
+
+    assert {
+        "distance": 0,
+        "start": 4,
+        "stop": 2,
+        "cigar": "4=",
+        "alignment": "TAAC\n||||\nTAAC",
+    } in results
+
+
+def test_find_two():
+    needle = "TAAC"
+    haystack = "TAACACGTTAACTA"
+
+    results = findall(needle, haystack, max_edits=0)
+
+    assert results == [
+        {
+            "distance": 0,
+            "start": 0,
+            "stop": 4,
+            "cigar": "4=",
+            "alignment": "TAAC\n||||\nTAAC",
+        },
+        {
+            "distance": 0,
+            "start": 8,
+            "stop": 12,
+            "cigar": "4=",
+            "alignment": "TAAC\n||||\nTAAC",
+        },
+    ]
+
+
+def test_find_overlap():
+    needle = "TATA"
+    haystack = "TATATA"
+
+    results = findall(needle, haystack, max_edits=0)
+
+    assert results == [
+        {
+            "distance": 0,
+            "start": 0,
+            "stop": 4,
+            "cigar": "4=",
+            "alignment": "TATA\n||||\nTATA",
+        },
+        {
+            "distance": 0,
+            "start": 2,
+            "stop": 6,
+            "cigar": "4=",
+            "alignment": "TATA\n||||\nTATA",
+        },
+    ]
